2801ef4cf8b32d18ea529bd4ff2fe9c1ff7a95df
Sean Kelly <skelly@saga.org>
2025-09-10 11:26:34 -0600

    KQ bug

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,4 +1,4 @@
 export type { IConfigManager, HasConfigType } from './i-config-manager.js';
-export { DotenvConfigManager } from './dotenv-config-manager';
+export { DotenvConfigManager } from './dotenv-config-manager.js';
 export { ConfigValidationError } from './config-validation-error.js';
 export { MockConfigManager } from './mocks/mock-config-manager.js';

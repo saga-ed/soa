6ad93ac3f00c14921c3731346c6d42441b45d967
Kyle Quinlan <kyle.quinlan6045@gmail.com>
2026-01-15 16:05:09 -0700

    Change name of export

--- packages/rabbitmq/src/index.ts
+++ packages/rabbitmq/src/index.ts
@@ -1,4 +1,4 @@
-export { ConnectionManager, type RabbitMQConfig as ConnectionConfig } from './connection-manager.js';
+export { ConnectionManager, type RabbitMQConfig } from './connection-manager.js';
 export { MessagePublisher } from './publisher.js';
 export { MessageConsumer } from './consumer.js';
 export type { QueueDefinition } from './queue.js';
